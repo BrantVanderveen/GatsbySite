33596d1e9138f18c68a9d7c5d46ecc3d34a21323
Brant Vanderveen <brantvanderveen@Vivians-iMac.local>
2019-05-07 11:45:36 -0400

    dotmap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,66 +1,67 @@
 import React from "react"
 import Link from "gatsby-link"
-import Counter from "./counter"
-import Wrapper from "./pagewrapper"
+import Layout from "../components/layout.js"
+import DotMap from '../components/dotmap.js'
 
-export default({data}) => (<Wrapper>
+export default({data}) => (
+  <Layout>
   <div>
-    <h1>
-      {data.site.siteMetadata.title}
-    </h1>
-    <p>
-      Test paragraph 1!
-    </p>
-    <p>
-      Test paragraph 2
-    </p>
-    <Link to="newPage">
-      Link to new page
-    </Link>
-    <br/>
-    <Counter color="blue"/>
-    <br/>
-    <div style={{
-        padding: '20px',
-        width: '50%',
-        minWidth: '500px',
-        color: "red"
-      }}>
-      <table>
-        <tbody>
-          <tr>
-            <th>path</th>
-            <th>size</th>
-            <th>extension</th>
-            <th>birth time</th>
-            <th>index</th>
-          </tr>
-          {
-            data.allFile.edges.map(({
-              node
-            }, i) => <tr>
-              <td>
-                {node.relativePath}
-              </td>
-              <td>
-                {node.prettySize}
-              </td>
-              <td>
-                {node.extension}
-              </td>
-              <td>
-                {node.birthTime}
-              </td>
-              <td>
-                {i}
-              </td>
-            </tr>)
-          }
-        </tbody>
-      </table>
-    </div>
+  <h1>
+    {data.site.siteMetadata.title}
+  </h1>
+  <p>
+    Test paragraph 1!
+  </p>
+  <p>
+    Test paragraph 2
+  </p>
+  <Link to="newPage">
+    Link to new page
+  </Link>
+  <br/>
+  <DotMap/>
+  <br/>
+  <div style={{
+      padding: '20px',
+      width: '50%',
+      minWidth: '500px',
+      color: "red"
+    }}>
+    <table>
+      <tbody>
+        <tr>
+          <th>path</th>
+          <th>size</th>
+          <th>extension</th>
+          <th>birth time</th>
+          <th>index</th>
+        </tr>
+        {
+          data.allFile.edges.map(({
+            node
+          }, i) => <tr>
+            <td>
+              {node.relativePath}
+            </td>
+            <td>
+              {node.prettySize}
+            </td>
+            <td>
+              {node.extension}
+            </td>
+            <td>
+              {node.birthTime}
+            </td>
+            <td>
+              {i}
+            </td>
+          </tr>)
+        }
+      </tbody>
+    </table>
   </div>
-</Wrapper>)
+</div>
+</Layout>)
 
 export const query = graphql `
   query FirstQuery {
